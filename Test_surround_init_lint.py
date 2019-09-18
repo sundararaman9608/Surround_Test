a5624277941a4bebfa35d3478518a0d06b3d1af2
sundararaman9608 <svedhagiri@deakin.edu.au>
2019-09-18 16:47:02 +1000

    testing files of surround environment

--- Test_surround_init_lint.py
+++ Test_surround_init_lint.py
@@ -1,31 +1,29 @@
 import unittest
 import subprocess
 import shutil
-
+import os
 
 class MyTestCase(unittest.TestCase):
-
 #validating the surround init command line functionality
 #verfiying the output after the surround init command
     def test_surround_init(self):
         process1= subprocess.run(["surround", "init"], encoding='utf-8', stdout=subprocess.PIPE,stdin=subprocess.PIPE)
         print("Output captured:", process1.stdout)
-        self.assertEqual(process1.stdout, "Name of project: ")
-
-#
+        self.assertIn(process1.stdout,"Name of project: ")
 
     def test_surround_init_lowercase(self):
-        process = subprocess.run(["surround", "init",'-p','tempo','-d','temporary','-w','no'], encoding='utf-8',
+        process = subprocess.run(["surround", "init",'-p','tempor','-d','temporary','-w','no'], encoding='utf-8',
                                  stdout=subprocess.PIPE,stdin=subprocess.PIPE)
         print("Output captured:", process.stdout)
-        self.assertIn(process.stdout, "info: project created at C:\\Users\\sunda\\Surround_Test\\tempo\n")
-        shutil.rmtree('tempo')
+        self.assertIn(process.stdout, "info: project created at C:\\Users\\sunda\\Surround_Test\\tempor\n")
+        shutil.rmtree('tempor')
 
     def test_surround_init_lowercase_underscore(self):
         process = subprocess.run(["surround", "init", '-p', 'tempo', '-d', 'temporary', '-w', 'no'], encoding='utf-8',
                                  stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         print("Output captured:", process.stdout)
         self.assertIn(process.stdout, "info: project created at C:\\Users\\sunda\\Surround_Test\\tempo\n")
+        self.assertTrue(os.path.exists("tempo"))
         shutil.rmtree('tempo')
 
     def test_surround_init_lowercase_symbol(self):
@@ -50,5 +48,17 @@ class MyTestCase(unittest.TestCase):
                    stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         print("Output captured:", process.stdout)
         self.assertIn(process.stdout, " TypeError: 'validator' should be of class Validator ")
+    def test_surround_lint(self):
+        process=subprocess.run(["surround", "lint"], encoding='utf-8',
+                   stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        print("Output captured:", process.stdout)
+        self.assertIn(process.stdout, " TypeError: 'validator' should be of class Validator ")
+    def test_surround_init_description_readme(self):
+        process = subprocess.run(["surround", "init", '-p','tempo','-d','temporary files in Read me','-w','no'], encoding='utf-8',
+                             stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        print("Output captured:", process.stdout)
+        self.assertIn(process.stdout, " TypeError: 'validator' should be of class Validator")
+        self.assertTrue(os.path.exists("tempo"))
+        shutil.rmtree('tempo')
 if __name__ == '__main__':
     unittest.main()
---
+++ tempo/tempo/stages.py
@@ -0,0 +1,19 @@
+from surround import Estimator, SurroundData, Validator
+
+
+class TempoData(SurroundData):
+    input_data = None
+    output_data = None
+
+
+class ValidateData(Validator):
+    def validate(self, surround_data, config):
+        if not surround_data.input_data:
+            raise ValueError("'input_data' is None")
+
+class Main(Estimator):
+    def estimate(self, surround_data, config):
+        surround_data.output_data = surround_data.input_data
+
+    def fit(self, surround_data, config):
+        print("TODO: Train your model here")
